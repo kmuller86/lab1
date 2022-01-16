6aded6d6b5c9a400d5afe63a7c88c44043ff79e9
kmuller86 <89707079+kmuller86@users.noreply.github.com>
2022-01-16 23:53:49 +0100

    Update main.java

--- cw6/src/main.java
+++ cw6/src/main.java
@@ -10,9 +10,18 @@ public class main {
         przypisać do niej wartości i wyświetlić */
         int[] tablica = new int[5];
         for (int i = 0; i < 5; i++)
+            //po co typuje pan i na int skoro i jest zawsze intem
             tablica [(int) i] = i +1;
         int zmienna = tablica[3];
         for (int i = 0; i < 5; i++)
             System.out.println("Kolejna komórka to: " + tablica[i]);
     }
 }
+
+/* miała być tablica double[];
+double[] tablica = new double[5];
+for (int i = 0; i < tablica.length; i++)
+     tablica [i] = i + 1.3;
+for (int i = 0; i < tablica.length; i++)
+     System.out.println("Kolejna komórka to: " + tablica[i]);
+*/
